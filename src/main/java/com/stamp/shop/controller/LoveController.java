266f5a05e0f77c0a3ac36b1ed57f5a208b32e0cc
obaibula <baibula.oo@gmail.com>
2024-02-26 17:39:47 +0200

    New commit

--- src/main/java/com/stamp/shop/controller/LoveController.java
+++ src/main/java/com/stamp/shop/controller/LoveController.java
@@ -7,9 +7,4 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/love")
 public class LoveController {
-
-    @GetMapping
-    public String love() {
-        return "I love Java ;)";
-    }
 }
