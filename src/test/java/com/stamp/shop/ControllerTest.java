ae3bc39079694d438fe829f2b569950d6a050378
Daniel Kleyman <denny_klm1@yahoo.com>
2024-02-26 16:29:36 +0200

    added controller and controller test

---
+++ src/test/java/com/stamp/shop/ControllerTest.java
@@ -0,0 +1,23 @@
+package com.stamp.shop;
+
+import com.stamp.shop.controllers.HelloWorldController;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+@WebMvcTest(HelloWorldController.class)
+public class ControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void testHello() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/hello"))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.content().string("Hello, World!"));
+    }
+}
